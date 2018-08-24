ae16bb59885431557549695b7595cab961c8d885
Ganesh Shinde <ganesh.shinde@globants-macbook-pro.local>
2018-08-24 15:39:54 +0530

    Added one more dependancy for Authentication.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return "{ 'name': 'Ganesh', 'mob':'0987654321'}"
     }
 
     // Example of configuring a controller
